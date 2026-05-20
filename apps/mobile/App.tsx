c53369336f33227fa7e5eab2884d0af0a8f747ce
sambhajingrdevelopers <kafarepradip806@gmail.com>
2026-05-20 12:02:46 +0530

    Update App.tsx

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
 import CreateProfileScreen from './src/screens/CreateProfileScreen';
 import MainTabs from './src/navigation/MainTabs';
+import MessagesScreen from './src/screens/MessagesScreen';
+import NotificationsScreen from './src/screens/NotificationsScreen';
+import PostDetailScreen from './src/screens/PostDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +22,9 @@ export default function App() {
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="CreateProfile" component={CreateProfileScreen} />
         <Stack.Screen name="MainTabs" component={MainTabs} />
+        <Stack.Screen name="Messages" component={MessagesScreen} />
+        <Stack.Screen name="Notifications" component={NotificationsScreen} />
+        <Stack.Screen name="PostDetail" component={PostDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
